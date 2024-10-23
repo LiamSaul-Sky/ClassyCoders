f3e0197df82cedd60079682da97070884536ca4e
LiamSaul-Sky <liam.saul@sky.uk>
2024-10-23 15:48:15 +0100

    Not working

--- classes/Employee.js
+++ classes/Employee.js
@@ -1,8 +1,38 @@
 class Employee {
+    #salary 
+    #isHired
+    static #allEmployees = [];
+    constructor(name, position, salary){
+        this.name = name;
+        this.position = position;
+        this.#salary = salary;
+        this.#isHired = true;
+    }
+    getSalary(){
+        return this.#salary
+    }
+    setSalary(amount){
+        this.#salary = amount;
+    }
+    getStatus(){
+        return this.#isHired;
+    }
+    setStatus(command){
+        if (command == "hire"){
+            this.#isHired = true;
+        } else if(command == "fire"){
+            this.#isHired = false;
+        }
+    }
+    }
+    static getEmployees(){
+        return this.#allEmployees;
+    }
+    static getTotalPayroll(){
+        for(let i = 0; i < getAllEmployees.length; i++){
 
-
-
-    
+        }
+    }
 }
 
 module.exports = {
--- classes/Manager.js
+++ classes/Manager.js
@@ -1,8 +1,18 @@
-class Manager {
+const { Employee } = require("./Employee");
 
-
-
-    
+class Manager extends Employee{
+    #employeesManaged
+    constructor(name, position, salary, department){
+        super(name, position, salary);
+        this.department = department;
+        this.#employeesManaged = [];
+    }
+    getEmployeesManaged(){
+        return this.#employeesManaged;
+    }
+    setEmployeesManaged(employee){
+        this.#employeesManaged.push(employee);
+    }
 }
 
 module.exports = {
